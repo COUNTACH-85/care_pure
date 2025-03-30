2b6083d00f453569e7b86db98108b47864ad5bc2
Dipankar Ghosh <dipankarghosh2006@gmail.com>
2025-03-30 22:07:31 +0530

    Fixed BlogPage

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import BlogCard from '../components/BlogCard';
-import Footer from '../components/FOOTER.JSX';
+import Footer from '../components/footer';
 
 const BlogPage = () => {
   // Predefined blog posts data about healthcare topics.
